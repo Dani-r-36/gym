7a4afbf47fd6aeeabc6c14c6d6b6b021a933096d
Dani-r-36 <danishanr0@gmail.com>
2023-06-22 17:34:13 +0100

    created whatsapp basic send and read

---
+++ create_tables_sql.py
@@ -0,0 +1,42 @@
+TABLES = """
+create table if not exists group_muscle(
+	group_id INT generated always as identity,
+	muscle_group VARCHAR(50) not null,
+	primary key (group_id)
+);
+
+create table if not exists muscle(
+	muscle_id INT generated always as identity,
+	muscle VARCHAR(50) not null,
+	group_id INT not null,
+	primary key (muscle_id),
+	foreign key (group_id) references group_muscle(group_id)
+);
+
+create table if not exists exercise(
+	exercise_id INT generated always as identity,
+	exercise_machine VARCHAR(100) not null,
+	muscle_id INT not null,
+	primary key (exercise_id),
+	foreign key (muscle_id) references muscle(muscle_id)
+);
+
+create table if not exists current_lift(
+	current_id INT generated always as identity,
+	max_working_weight VARCHAR(10) not null,
+	max_reps INT not null,
+	primary key (current_id)
+);
+
+create table if not exists exercise_details(
+	exercise_details_id INT generated always as identity,
+	exercise_id INT not null,
+	current_id INT not null,
+	intensity VARCHAR(20) not null,
+	tips VARCHAR(200) not null,
+	optimum_level VARCHAR(20) not null,
+	picture_video_link VARCHAR(50) not null,
+	primary key (exercise_details_id),
+	foreign key (exercise_id) references exercise(exercise_id),
+	foreign key (current_id) references current_lift(current_id)
+);"""
---
+++ whatsapp.py
@@ -0,0 +1,72 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+import time
+
+def connect_whatsapp():
+    driver = webdriver.Chrome()
+    driver.get('https://web.whatsapp.com/')
+    # WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.message-in")))
+    time.sleep(15)
+    # WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.message-in")))
+    # time.sleep(15)
+    contact_name = "Gym"
+    search_box = driver.find_element(By.XPATH, '//*[contains(@class, "selectable-text copyable-text")]')
+    search_box.send_keys(contact_name)
+    time.sleep(2)  # Wait for search results to load
+    chat = driver.find_element(By.XPATH, f"//span[@title='{contact_name}']")
+    chat.click()
+    time.sleep(2)
+    return driver
+
+def send_message(driver, message):
+
+    inp_xpath = '//p[@class, "selectable-text copyable-text"][@contenteditable="true"][@data-tab="1"]'
+    input_box = driver.find_element(By.CSS_SELECTOR, "div[contenteditable='true'][data-tab='10']")
+    time.sleep(2)
+    input_box.send_keys(message + Keys.ENTER)
+
+def start_sess(driver):
+    print("riunning")
+    time.sleep(5)
+    # message_elements = driver.find_elements(By.CSS_SELECTOR, "div.message-out")
+    message_elements = driver.find_elements(By.CSS_SELECTOR, "div.message-out")
+    time.sleep(5)
+    last_message_element = message_elements[-1]
+    # Extract the text content of each message
+    message_text = last_message_element.text
+    message = message_text.strip().split("\n")
+    message = message[0]
+    if message == "sess":
+        record_sess(driver)
+        # print(message_text)
+
+def record_sess(driver):
+    print("running sess")
+    send_message(driver, "What muscle?")
+    time.sleep(2)
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.message-out")))
+    muscle = last_message(driver)
+    print(muscle)
+    print("finished reacording")
+
+def last_message(driver):
+    print("read last")
+    last_message = "What muscle?"
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.message-out")))
+    while last_message == "What muscle?":
+        message_elements = driver.find_elements(By.CSS_SELECTOR, "div.message-out:last-of-type")
+        last_message_element = message_elements[-1]
+        # Extract the text content of the last message
+        last_message_text = last_message_element.text
+        last_message = last_message_text.strip().split("\n")
+        last_message = last_message[0]
+    return last_message
+
+if __name__ == "__main__":
+    driver = connect_whatsapp()
+    while True:
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.message-out")))
+        start_sess(driver)
